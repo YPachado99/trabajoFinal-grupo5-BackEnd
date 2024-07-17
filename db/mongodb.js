0852e2a74281bc3d41c24251ff50ce7d1ac8ac6f
YPachado99 <jonathanpachado73@gmail.com>
2024-07-17 01:46:03 -0300

    fix conexion con base de datos

--- db/mongodb.js
+++ db/mongodb.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
-const { DB_USER, DB_PASSWORD, DB_CLUSTER, DB_NAME } = process.env;
+require("dotenv").config(); // Para cargar las variables de entorno desde .env
+
+const { DB_USER, DB_PASSWORD,DB_NAME } = process.env;
 
 // Conectar a la base de datos
 
 const connectDb = async () => {
   console.log("Esperando Conexion con la Base de Datos");
   try {
-    await mongoose.connect("mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/?retryWrites=true&w=majority&appName=BDcodeStockers", {
+    await mongoose.connect("mongodb+srv://${DB_USER}:${DB_PASSWORD}@bdcodestockers.uaqhnmp.mongodb.net/?retryWrites=true&w=majority&appName=DB_NAME", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
