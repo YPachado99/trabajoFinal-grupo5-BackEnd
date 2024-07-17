ec1775fa3b86bb11d4c24ee98c9b5d7614fbe7ff
YPachado99 <jonathanpachado73@gmail.com>
2024-07-17 02:18:02 -0300

    corrigiendo errores

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
-const express = require("express"); 
-const app = express(); 
-const connectDb = require("./db/mongodb");
-const cors = require("cors");
-const comprobacionJwt = require("./middleware/comprobacionJwt");
-const dotenv = require("dotenv");
-dotenv.config();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: "https://codestockers.netlify.app", // permite solicitudes desde este origen
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // métodos HTTP permitidos
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ["Content-Type", "Authorization"], // cabeceras permitidas
-  credentials: true, // habilita el intercambio de cookies entre dominios
-}));
+const express = require("express");  
+const app = express();  
+const connectDb = require("./db/mongodb");  
+const cors = require("cors");  
+const comprobacionJwt = require("./middleware/comprobacionJwt");  
+const dotenv = require("dotenv");  
+dotenv.config();  
 
+app.use(express.json());  
+app.use(express.urlencoded({ extended: true }));  
 
-dotenv.config();
-const PORT = process.env.PORT || 3000;
-connectDb();
-const initApp = async () => {
-  try {
-    app.listen(PORT, () => {
-      console.log(`Servidor puesto en marcha en el puerto ${PORT}`);
-    });
-    await connectDb();
-  } catch (error) {
-    console.log("Error al iniciar la aplicacion ");
-  }
-};
+app.use(cors({  
+  origin: "https://codestockers.netlify.app",  
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",  
+  preflightContinue: false,  
+  optionsSuccessStatus: 204,  
+  allowedHeaders: ["Content-Type", "Authorization"],  
+  credentials: true,  
+}));  
 
-initApp();
+const PORT = process.env.PORT || 3000;  
 
-//creo una ruta en express
+const initApp = async () => {  
+  try {  
+    await connectDb(); // Conexión a la base de datos antes de iniciar la aplicación  
+    app.listen(PORT, () => {  
+      console.log(`Servidor puesto en marcha en el puerto ${PORT}`);  
+    });  
+    
+    // Rutas de la aplicación  
+    app.use("/api/user", require("./routes/RutasUsuarios"));  
+    app.use("/api", require("./routes/RutasProductos"));  
+    app.use("/protegida", comprobacionJwt, require("./routes/RutaAdmin"));  
+    
+  } catch (error) {  
+    console.log("Error al iniciar la aplicación");  
+  }  
+};  
 
-app.use("/api/user", require("./routes/RutasUsuarios"));
-app.use("/api", require("./routes/RutasProductos"));
-
-app.use("/protegida", comprobacionJwt, require("./routes/RutaAdmin"));
+initApp();  
