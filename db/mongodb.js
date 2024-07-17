06615d227034e0372892817a12537cad628c6a56
YPachado99 <jonathanpachado73@gmail.com>
2024-07-17 02:33:10 -0300

    fix

--- db/mongodb.js
+++ db/mongodb.js
@@ -1,14 +1,11 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config()
-
-const { DB_USER, DB_PASSWORD,DB_NAME } = process.env;
 
 // Conectar a la base de datos
+
 const connectDb = async () => {
   console.log("Esperando Conexion con la Base de Datos");
   try {
-    await mongoose.connect( `mongodb+srv://${DB_USER}:${DB_PASSWORD}@bdcodestockers.uaqhnmp.mongodb.net/?retryWrites=true&w=majority&appName=BDcodeStockers`, {
+    await mongoose.connect("mongodb+srv://root:root@bdcodestockers.uaqhnmp.mongodb.net/?retryWrites=true&w=majority&appName=BDcodeStockers", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require("express"); 
-const app = express();  
-const connectDb = require("./db/mongodb");  
+const app = express(); 
+const connectDb = require("./db/mongodb");
 const cors = require("cors");
 const comprobacionJwt = require("./middleware/comprobacionJwt");
-const dotenv = require("dotenv");
-dotenv.config();
 
-app.use(express.json());  
-app.use(express.urlencoded({ extended: true }));  
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: "https://codestockers.netlify.app", // permite solicitudes desde este origen
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // métodos HTTP permitidos
@@ -16,12 +13,11 @@ app.use(cors({
   optionsSuccessStatus: 204,
   allowedHeaders: ["Content-Type", "Authorization"], // cabeceras permitidas
   credentials: true, // habilita el intercambio de cookies entre dominios
-}));  
+}));
 
-
+require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 connectDb();
-
 const initApp = async () => {
   try {
     app.listen(PORT, () => {
@@ -36,6 +32,8 @@ const initApp = async () => {
 initApp();
 
 //creo una ruta en express
+
 app.use("/api/user", require("./routes/RutasUsuarios"));
 app.use("/api", require("./routes/RutasProductos"));
+
 app.use("/protegida", comprobacionJwt, require("./routes/RutaAdmin"));
