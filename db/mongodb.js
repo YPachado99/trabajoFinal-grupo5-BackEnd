779c1d429ca3e209d104894280045f1d2832498f
YPachado99 <jonathanpachado73@gmail.com>
2024-07-17 01:32:03 -0300

    configurando el .env

--- db/mongodb.js
+++ db/mongodb.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
+const { DB_USER, DB_PASSWORD, DB_CLUSTER, DB_NAME } = process.env;
 
 // Conectar a la base de datos
 
 const connectDb = async () => {
   console.log("Esperando Conexion con la Base de Datos");
   try {
-    await mongoose.connect("mongodb+srv://root:root@bdcodestockers.uaqhnmp.mongodb.net/?retryWrites=true&w=majority&appName=BDcodeStockers", {
+    await mongoose.connect("mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/?retryWrites=true&w=majority&appName=BDcodeStockers", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
